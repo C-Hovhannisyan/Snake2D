29a70b491e60a49280fc947fa89ec41757b774c6
C.Hovhannisyan <qristin.hovhannisyan13@gmail.com>
2022-10-14 17:31:03 +0400

    Init project commit

---
+++ Snake2D/Assets/GameOverScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+
+    public Text scoreText;
+  public void Setup(int score)
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("Snake");
+    }
+
+    
+}
---
+++ Snake2D/Assets/ScoreManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+
+    public Text scoreText;
+    public Text highscoreText;
+
+    int score = 0;
+    int highscore = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        highscore = PlayerPrefs.GetInt("highscore", 0);
+
+        scoreText.text = "Your score is: " + score.ToString();
+        highscoreText.text = "Highscore: " + highscore.ToString();
+
+    }
+
+   public void AddScore()
+    {
+        score += 10;
+        scoreText.text = "Your score is: " + score.ToString();
+
+        if (highscore < score)
+        {
+            PlayerPrefs.SetInt("highscore", score);
+        }
+
+
+    }
+}
+
